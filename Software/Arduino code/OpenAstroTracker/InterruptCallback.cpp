d2a1fd70d06da8392daa9a1bd162c64106e00a0e
Unknown <jwellman@gmail.com>
2020-06-12 11:25:18 -0500

    Timers for ESP32, still bad pins for ESP32

--- Software/Arduino code/OpenAstroTracker/InterruptCallback.cpp
+++ Software/Arduino code/OpenAstroTracker/InterruptCallback.cpp
@@ -19,7 +19,9 @@
   #define USE_TIMER_5     false
   #include "TimerInterrupt.h"
 #else
-  #error Unrecognized board selected. Either implement interrupt code or define the board here.
+  // Need to add support for ESP32 boards here
+
+  #error Only Arduino Uno, Mega and ESP8266 boards currently supported.
 #endif
 
 #ifdef ESP8266
@@ -56,63 +58,11 @@ void InterruptCallback::stop()
 {
 }
 
-#elif defined(ESP32)
-
-/*
-volatile bool _lock = false;
-
-void IRAM_ATTR callbackProxy() {
-    if (_lock) {
-        return;
-    }
-
-    timerAlarmDisable(_timer);
-    _lock = true;
-    if (func != NULL) {
-        func(data);
-    }
-    _lock = false;
-    timerAlarmEnable(_timer);
-}
-
-bool InterruptCallback::setInterval(float intervalMs, interrupt_callback_p callback, void* payload){
-    func = callback;
-    data = payload;
-
-    // 80 divisor = 1uS resolution.
-    _timer = timerBegin(0, 80, true);
-    timerAttachInterrupt(_timer, callbackProxy, true);
-    timerAlarmWrite(_timer, (uint64_t)(intervalMs * 1000.0f), true);
-    timerAlarmEnable(_timer);
-
-#ifdef DEBUG_MODE
-    Serial.println("Setup ESP32 Timer");
-#endif // DEBUG_MODE
-
-}
-
-void InterruptCallback::stop(){
-#ifdef DEBUG_MODE
-    Serial.println("Stop ESP32 Timer");
-#endif // DEBUG_MODE
-    if (timerAlarmEnabled(_timer)) {
-        timerAlarmDisable(_timer);
-    }
-}
-
-void InterruptCallback::start(){
-#ifdef DEBUG_MODE
-    Serial.println("Start ESP32 Timer");
-#endif // DEBUG_MODE
-    if (!timerAlarmEnabled(_timer)){
-        timerAlarmEnable(_timer);
-    }
-}
-*/
+#else
 
 #elif defined __AVR_ATmega328P__ || defined __AVR_ATmega2560__
 
-  bool InterruptCallback::setInterval(float intervalMs, interrupt_callback_p callback, void* payload)
+bool InterruptCallback::setInterval(float intervalMs, interrupt_callback_p callback, void* payload)
 {
   // We have requested to use Timer2 (see above)
   ITimer2.init();
