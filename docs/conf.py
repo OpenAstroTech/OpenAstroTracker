fb600860208b497596c23d5ad742cb6dbe03f31e
OpenAstroTech <fabian@openastrotech.com>
2020-06-16 23:05:11 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = 'Fabian Uehleke'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
+extensions = ['recommonmark'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -50,3 +50,6 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Needed for RTD build
+master_doc = 'index'
