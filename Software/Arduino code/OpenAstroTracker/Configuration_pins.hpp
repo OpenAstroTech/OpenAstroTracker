308a0b3f82951cc9539fa4a7019a85a127707cfb
ClutchplateDude <lutz@stmuc.com>
2020-08-01 22:04:17 -0700

    V1.8.02 - Updates
    - Cleaned up some pin definitions (ESP32 was incorrect, amongst other things)

--- Software/Arduino code/OpenAstroTracker/Configuration_pins.hpp
+++ Software/Arduino code/OpenAstroTracker/Configuration_pins.hpp
@@ -1,59 +1,96 @@
 
+////////////////////////////////////////////////////////////////////
 // Arduino MEGA 
+////////////////////////////////////////////////////////////////////
 #ifdef __AVR_ATmega2560__  // Arduino Mega
-//---------------------------------------
-// If using 28BYJ-48 and ULN2003 drivers:
-#define RA_IN1_PIN  22
-#define RA_IN2_PIN  24
-#define RA_IN3_PIN  26
-#define RA_IN4_PIN  28
-#define DEC_IN1_PIN 30
-#define DEC_IN2_PIN 32
-#define DEC_IN3_PIN 34
-#define DEC_IN4_PIN 36
-//-----------------------
-// If using NEMA steppers
-#define RA_STEP_PIN 22  // STEP
-#define RA_DIR_PIN  24  // DIR
-#define RA_EN_PIN   25  // Enable
-#define RA_MS0_PIN  30, HIGH    // 
-#define RA_MS1_PIN  32, HIGH
-#define RA_MS2_PIN  34, HIGH
-//RA TMC2209 UART specific pins
-#define RA_SERIAL_PORT Serial3  // HardwareSerial port, wire to TX3 for write-only
-#define RA_DRIVER_ADDRESS 0b00  // Set by MS1/MS2. LOW/LOW in this case
-#define RA_DIAG_PIN 40          // only needed for autohome function
+    //---------------------------------------
+    // If using 28BYJ-48 and ULN2003 drivers:
+    #define RA_IN1_PIN  22
+    #define RA_IN2_PIN  24
+    #define RA_IN3_PIN  26
+    #define RA_IN4_PIN  28
+    #define DEC_IN1_PIN 30
+    #define DEC_IN2_PIN 32
+    #define DEC_IN3_PIN 34
+    #define DEC_IN4_PIN 36
+    //-----------------------
+    // If using NEMA steppers
+    #define RA_STEP_PIN 22  // STEP
+    #define RA_DIR_PIN  24  // DIR
+    #define RA_EN_PIN   25  // Enable
+    #define RA_MS0_PIN  30, HIGH    // 
+    #define RA_MS1_PIN  32, HIGH
+    #define RA_MS2_PIN  34, HIGH
+    //RA TMC2209 UART specific pins
+    #define RA_SERIAL_PORT Serial3  // HardwareSerial port, wire to TX3 for write-only
+    #define RA_DRIVER_ADDRESS 0b00  // Set by MS1/MS2. LOW/LOW in this case
+    #define RA_DIAG_PIN 40          // only needed for autohome function
 
-#define DEC_STEP_PIN 26  // STEP
-#define DEC_DIR_PIN  28  // DIR
-#define DEC_EN_PIN   29  // Enable
-#define DEC_MS0_PIN  31  // 
-#define DEC_MS1_PIN  33
-#define DEC_MS2_PIN  35
-//DEC TMC2209 UART specific pins
-#define DEC_SERIAL_PORT Serial3  // HardwareSerial port, wire to TX2 for write-only
-#define DEC_DRIVER_ADDRESS 0b01  // Set by MS1/MS2 (MS1 HIGH, MS2 LOW)
-#define DEC_DIAG_PIN 41          // only needed for autohome function
-//---------------
-// MISC PINS
-#define GPS_SERIAL_PORT Serial1
-#define GPS_BAUD_RATE 9600
+    #define DEC_STEP_PIN 26  // STEP
+    #define DEC_DIR_PIN  28  // DIR
+    #define DEC_EN_PIN   29  // Enable
+    #define DEC_MS0_PIN  31  // 
+    #define DEC_MS1_PIN  33
+    #define DEC_MS2_PIN  35
+    //DEC TMC2209 UART specific pins
+    #define DEC_SERIAL_PORT Serial3  // HardwareSerial port, wire to TX2 for write-only
+    #define DEC_DRIVER_ADDRESS 0b01  // Set by MS1/MS2 (MS1 HIGH, MS2 LOW)
+    #define DEC_DIAG_PIN 41          // only needed for autohome function
+    //---------------
+    // MISC PINS
+    #define GPS_SERIAL_PORT Serial1
+    #define GPS_BAUD_RATE 9600
 #endif //mega
 
+////////////////////////////////////////////////////////////////////
 // ESP32
+////////////////////////////////////////////////////////////////////
 #ifdef ESP32
-// todo
+  #if (RA_STEPPER_TYPE != STEP_28BYJ48) || (DEC_STEPPER_TYPE != STEP_28BYJ48)
+    #error Only 28BJY-48 stepper motors are supported on the ESP (for now)
+  #endif
+
+  // RA Motor pins
+  #define RA_IN1_PIN  19  
+  #define RA_IN2_PIN  21  
+  #define RA_IN3_PIN  22  
+  #define RA_IN4_PIN  23  
+
+  // DEC Motor pins
+  #define DEC_IN1_PIN  16  
+  #define DEC_IN2_PIN  17  
+  #define DEC_IN3_PIN  5   
+  #define DEC_IN4_PIN  18  
+
+// ST4 Input Pins - TODO.
+    /*#define st4North      SD0 
+    #define st4South      SD1
+    #define st4West       SD2
+    #define st4East       SD3*/
+    #if USE_GPS == 1
+        #error GPS module not currently configured/supported in ESP32
+    #endif
 #endif
 
+////////////////////////////////////////////////////////////////////
 // Arduino UNO
+////////////////////////////////////////////////////////////////////
 #ifdef __AVR_ATmega328P__ // Arduino Uno
-#define RA_IN1_PIN  12
-#define RA_IN2_PIN  11
-#define RA_IN3_PIN  3
-#define RA_IN4_PIN  2
+  #if (RA_STEPPER_TYPE != STEP_28BYJ48) || (DEC_STEPPER_TYPE != STEP_28BYJ48)
+    #error Only 28BJY-48 stepper motors are supported on the UNO (for now)
+  #endif
 
-#define DEC_IN1_PIN 18
-#define DEC_IN2_PIN 17
-#define DEC_IN3_PIN 16
-#define DEC_IN4_PIN 15
+  #define RA_IN1_PIN  12
+  #define RA_IN2_PIN  11
+  #define RA_IN3_PIN  3
+  #define RA_IN4_PIN  2
+
+  #define DEC_IN1_PIN 18
+  #define DEC_IN2_PIN 17
+  #define DEC_IN3_PIN 16
+  #define DEC_IN4_PIN 15
+
+  #if USE_GPS == 1
+    #error GPS module not currently configured/supported in ESP32
+  #endif
 #endif
--- Software/Arduino code/OpenAstroTracker/a_inits.hpp
+++ Software/Arduino code/OpenAstroTracker/a_inits.hpp
@@ -1,23 +1,23 @@
 #pragma once
 
-
 #include <AccelStepper.h>
 #include <LiquidCrystal.h>
-
-#if USE_GPS == 1
-//#include <SoftwareSerial.h>
-//SoftwareSerial SoftSerial(50, 52); // RX, TX
-#endif
+#include "configuration_pins.hpp"
 
 #include "Utility.hpp"
 #include "DayTime.hpp"
 #include "Mount.hpp"
 #include "MeadeCommandProcessor.hpp"
+
 #if RA_DRIVER_TYPE == TMC2009_UART
   #include <TMCStepper.h>
 #endif
+
 #if USE_GPS == 1
+  //#include <SoftwareSerial.h>
   #include <TinyGPS++.h>
+
+  //SoftwareSerial SoftSerial(GPS_SERIAL_RX_PIN, GPS_SERIAL_TX_PIN); // RX, TX
   TinyGPSPlus gps;
 #endif
 
@@ -25,131 +25,10 @@
 #define FULLSTEP 4
 #define DRIVER 1
 
-///////////////////////////////////////////////////////////////////////////
-// ESP8266 Wifi Board (NodeMCU)
-///////////////////////////////////////////////////////////////////////////
-#ifdef ESP8266
-// RA Motor pins
-  #if INVERT_RA_DIR == 1
-    #define RAmotorPin1  D0    // IN1 auf ULN2003 driver 1
-    #define RAmotorPin3  D1    // IN2 auf ULN2003 driver 1
-    #define RAmotorPin2  D2    // IN3 auf ULN2003 driver 1
-    #define RAmotorPin4  D3    // IN4 auf ULN2003 driver 1
-  #else
-    #define RAmotorPin1  D3    // IN1 auf ULN2003 driver 1
-    #define RAmotorPin3  D2    // IN2 auf ULN2003 driver 1
-    #define RAmotorPin2  D1    // IN3 auf ULN2003 driver 1
-    #define RAmotorPin4  D0    // IN4 auf ULN2003 driver 1
-  #endif
-
-// DEC Motor pins
-  #if INVERT_DEC_DIR == 1
-    #define DECmotorPin1  D5    // IN1 auf ULN2003 driver 2
-    #define DECmotorPin3  D6    // IN3 auf ULN2003 driver 2
-    #define DECmotorPin2  D7    // IN2 auf ULN2003 driver 2
-    #define DECmotorPin4  D8    // IN4 auf ULN2003 driver 2
-  #else
-    #define DECmotorPin1  D8    // IN1 auf ULN2003 driver 2
-    #define DECmotorPin3  D7    // IN3 auf ULN2003 driver 2
-    #define DECmotorPin2  D6    // IN2 auf ULN2003 driver 2
-    #define DECmotorPin4  D5    // IN4 auf ULN2003 driver 2
-  #endif
-
-// ST4 Input Pins - TODO.
-    #define st4North      SD0 
-    #define st4South      SD1
-    #define st4West       SD2
-    #define st4East       SD3
-#endif
-
-
-///////////////////////////////////////////////////////////////////////////
-// ESP32 Wifi Board
-///////////////////////////////////////////////////////////////////////////
-#ifdef ESP32
-// RA Motor pins
-  #if INVERT_RA_DIR == 1
-    #define RAmotorPin1  35    // IN1 auf ULN2003 driver 1
-    #define RAmotorPin3  34    // IN2 auf ULN2003 driver 1
-    #define RAmotorPin2  39    // IN3 auf ULN2003 driver 1
-    #define RAmotorPin4  36    // IN4 auf ULN2003 driver 1
-  #else
-    #define RAmotorPin1  23    // IN1 auf ULN2003 driver 1
-    #define RAmotorPin3  22    // IN2 auf ULN2003 driver 1
-    #define RAmotorPin2  21    // IN3 auf ULN2003 driver 1
-    #define RAmotorPin4  19    // IN4 auf ULN2003 driver 1
-  #endif
-
-// DEC Motor pins
-  #if INVERT_DEC_DIR == 1
-    #define DECmotorPin1  26    // IN1 auf ULN2003 driver 2
-    #define DECmotorPin3  25    // IN3 auf ULN2003 driver 2
-    #define DECmotorPin2  33    // IN2 auf ULN2003 driver 2
-    #define DECmotorPin4  32    // IN4 auf ULN2003 driver 2
-  #else
-    #define DECmotorPin1  18    // IN1 auf ULN2003 driver 2
-    #define DECmotorPin3  5     // IN3 auf ULN2003 driver 2
-    #define DECmotorPin2  17    // IN2 auf ULN2003 driver 2
-    #define DECmotorPin4  16    // IN4 auf ULN2003 driver 2
-  #endif
-
-// ST4 Input Pins - TODO.
-    /*#define st4North      SD0 
-    #define st4South      SD1
-    #define st4West       SD2
-    #define st4East       SD3*/
-#endif
-
-///////////////////////////////////////////////////////////////////////////
-// Arduino Uno
-///////////////////////////////////////////////////////////////////////////
-#ifdef __AVR_ATmega328P__ // normal Arduino Mapping
+////////////////////////////////////
+// Stepper definitions /////////////
 #if RA_STEPPER_TYPE == STEP_28BYJ48
-// RA Motor pins
-  #if INVERT_RA_DIR == 1
-    #define RAmotorPin1  RA_IN1_PIN    // IN1 auf ULN2003 driver 1
-    #define RAmotorPin3  RA_IN2_PIN    // IN2 auf ULN2003 driver 1
-    #define RAmotorPin2  RA_IN3_PIN     // IN3 auf ULN2003 driver 1
-    #define RAmotorPin4  RA_IN4_PIN     // IN4 auf ULN2003 driver 1
-  #else
-    #define RAmotorPin1  RA_IN4_PIN     // IN1 auf ULN2003 driver 1
-    #define RAmotorPin3  RA_IN3_PIN     // IN2 auf ULN2003 driver 1
-    #define RAmotorPin2  RA_IN2_PIN    // IN3 auf ULN2003 driver 1
-    #define RAmotorPin4  RA_IN1_PIN    // IN4 auf ULN2003 driver 1
-  #endif
-#endif
-#if RA_STEPPER_TYPE == STEP_NEMA17
-    #define RAmotorPin1 11
-    #define RAmotorPin2 12
-#endif
-
-// DEC Motor pins
-#if DEC_STEPPER_TYPE == STEP_28BYJ48
-  #if INVERT_DEC_DIR == 1
-    #define DECmotorPin1  DEC_IN1_PIN    // IN1 auf ULN2003 driver 2
-    #define DECmotorPin3  DEC_IN2_PIN    // IN2 auf ULN2003 driver 2
-    #define DECmotorPin2  DEC_IN3_PIN    // IN3 auf ULN2003 driver 2
-    #define DECmotorPin4  DEC_IN4_PIN    // IN4 auf ULN2003 driver 2
-  #else
-    #define DECmotorPin1  DEC_IN4_PIN    // IN1 auf ULN2003 driver 2
-    #define DECmotorPin3  DEC_IN3_PIN    // IN3 auf ULN2003 driver 2
-    #define DECmotorPin2  DEC_IN2_PIN    // IN2 auf ULN2003 driver 2
-    #define DECmotorPin4  DEC_IN1_PIN    // IN4 auf ULN2003 driver 2
-  #endif
-#endif
-#if DEC_STEPPER_TYPE == STEP_NEMA17
-    #define DECmotorPin1  16
-    #define DECmotorPin2  17
-#endif
-
-#endif
-
-///////////////////////////////////////////////////////////////////////////
-// Arduino Mega
-///////////////////////////////////////////////////////////////////////////
-#ifdef __AVR_ATmega2560__  // Arduino Mega
-#if RA_STEPPER_TYPE == STEP_28BYJ48
-// RA Motor pins
+  // RA Motor pins
   #if INVERT_RA_DIR == 1
     #define RAmotorPin1  RA_IN1_PIN    // IN1 auf ULN2003 driver 1
     #define RAmotorPin3  RA_IN2_PIN    // IN2 auf ULN2003 driver 1
@@ -161,8 +40,7 @@
     #define RAmotorPin2  RA_IN2_PIN    // IN3 auf ULN2003 driver 1
     #define RAmotorPin4  RA_IN1_PIN    // IN4 auf ULN2003 driver 1
   #endif
-#endif
-#if RA_STEPPER_TYPE == STEP_NEMA17
+#elif RA_STEPPER_TYPE == STEP_NEMA17
     #define RAmotorPin1  RA_STEP_PIN
     #define RAmotorPin2  RA_DIR_PIN
 #endif
@@ -180,22 +58,23 @@
     #define DECmotorPin2  DEC_IN3_PIN    // IN3 auf ULN2003 driver 2
     #define DECmotorPin4  DEC_IN4_PIN    // IN4 auf ULN2003 driver 2
   #endif
-#endif
-#if DEC_STEPPER_TYPE == STEP_NEMA17
+#elif DEC_STEPPER_TYPE == STEP_NEMA17
     #define DECmotorPin1  DEC_STEP_PIN
     #define DECmotorPin2  DEC_DIR_PIN
 #endif
-  
-#endif
+// End Stepper Definitions //////////////
+/////////////////////////////////////////
 
-// Driver definitions /////////////
+/////////////////////////////////////////
+// Driver definitions ///////////////////
 #if RA_DRIVER_TYPE == TMC2009_STANDALONE
   //#define RA_EN_PIN 40  // Enable Pin
 #endif
 #if RA_DRIVER_TYPE == TMC2009_UART 
   #define R_SENSE 0.11f           // 0.11 for StepStick
 #endif
-// End Driver Definitions //////////////
+// End Driver Definitions ///////////////
+/////////////////////////////////////////
 
 
 // Menu IDs
