5f14523700eadca257b88a207e7d7d0db12dccd5
Cameron Tetford <cameron@tetford.me>
2020-08-17 10:40:03 -0400

    AutoPA Update
    Updated programs for AutoPA calibration and manual control

--- Software/Addons/AutoPA/polaralign_manualcalibration.py
+++ Software/Addons/AutoPA/polaralign_manualcalibration.py
@@ -5,6 +5,7 @@ from astropy import units as u
 from astropy.time import Time
 from astropy.utils import iers
 import sys, subprocess
+import argparse
 
 def polarcalibrate(mylat, mylong, myelev):
 	#iers.conf.auto_download = False
@@ -20,8 +21,8 @@ def polarcalibrate(mylat, mylong, myelev):
 	p2DEC = input("Enter second DEC value in 00d00m00.0s format: ")
 	p2Time = Time(input("Enter second Time value in YYYY-MM-DD HH:MM:SS format: "))
 
-	p1 = SkyCoord(p1RA, p1DEC)
-	p2 = SkyCoord(p2RA, p2DEC)
+	p1 = SkyCoord(p1RA, p1DEC, frame='itrs', unit='deg', representation_type='spherical', obstime=Time(p1Time))
+	p2 = SkyCoord(p2RA, p2DEC, frame='itrs', unit='deg', representation_type='spherical', obstime=Time(p2Time))
 	
 	p1AzAlt = p1.transform_to(AltAz(obstime=p1Time,location=observing_location))
 	p2AzAlt = p2.transform_to(AltAz(obstime=p2Time,location=observing_location))
@@ -37,21 +38,36 @@ def polarcalibrate(mylat, mylong, myelev):
 	else:
 		p2Az = p2AzAlt.az.deg*60
 		
-	deltaAz = abs(p1Az - p2Az)
-	deltaAlt = abs(p1AzAlt.alt.arcmin - p2AzAlt.alt.arcmin)
+	deltaAz = abs(p2Az - p1Az)
+	deltaAlt = abs(p2AzAlt.alt.arcmin - p1AzAlt.alt.arcmin)
 	
 	print(f"Azimuth delta is: {deltaAz:.4f} arcminutes.")
 	print(f"Altitude delta is: {deltaAlt:.4f} arcminutes.")
 	
 	return
 
-#Latitude in degrees
-mylat = float(sys.argv[1])
-
-#Longitude in degrees
-mylong = float(sys.argv[2])
-
-#Elevation in meters
-myelev = float(sys.argv[3])
+parser = argparse.ArgumentParser(usage='%(prog)s [mylat] [mylong] [myelev]', description='OpenAstroTracker AutoPA Manual Calibration: \
+        This tool is used to measure the alt/az distance between RA/DEC values (Entered manually. \
+        Values can be obtained via uploading two images to https://nova.astrometry.net/upload).\
+        It can be used to calibrate the arcminutes/step values by comparing the actual measured movement compared to any movement made using the LCD controls.')
+parser.add_argument("mylat", help="Your latitude in degrees", type=float)
+parser.add_argument("mylong", help="Your longitude in degrees", type=float)
+parser.add_argument("myelev", help="Your elevation in metres", type=float)
+args = parser.parse_args()
+mylat = args.mylat
+mylong = args.mylong
+myelev = args.myelev
+if args.serialport:
+    serialport = args.serialport
+else:
+	serialport = "/dev/ttyACM0"
+if args.existRA:
+    existRA = args.existRA
+else:
+	existRA = "-"
+if args.existDEC:
+    existDEC = args.existDEC
+else:
+	existDEC = "-"
 
 polarcalibrate(mylat, mylong, myelev)
