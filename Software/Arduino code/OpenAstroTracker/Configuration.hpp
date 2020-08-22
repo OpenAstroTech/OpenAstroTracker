465f8eef2fbabe1cc734369abe488f48f15174b2
Andre Stefanov <mail@andrestefanov.de>
2020-08-22 15:51:41 +0300

    Moved project description to .ino file

--- Software/Arduino code/OpenAstroTracker/Configuration.hpp
+++ Software/Arduino code/OpenAstroTracker/Configuration.hpp
@@ -1,22 +1,6 @@
 #pragma once
 #include "../Configuration_adv.hpp"   // <-- 
 #include "../Configuration_pins.hpp"  // <-- ignore these
-/*
-  =======================================================================================================================================
-
-    Version 1.8
-
-    1. Connect your Arduino, under tools choose the board you're using (e.g. Arduino Mega), set the right Port and set "Arduino ISP" as the Programmer.
-    2. Hit upload (Ctrl-U)
-
-    You might need to download the "AccelStepper" library by Mike McCauley
-
-    Authors: /u/intercipere
-             /u/clutchplate
-             /u/EorEquis
-
-  =======================================================================================================================================
-*/
 
 ///////////////////////////////////////////////////////////////////////////
 // Also use Configuration_adv for further adjustments!
