4e02fd8c5bca08a8ebd6496e2a7856710d6f1aec
phil <36334336+philharrisathome@users.noreply.github.com>
2020-12-05 18:35:08 +0000

    Verify presence, reduce bandwidth, decorrelated measurements

--- Software/Arduino code/OpenAstroTracker/src/Gyro.cpp
+++ Software/Arduino code/OpenAstroTracker/src/Gyro.cpp
@@ -8,30 +8,34 @@
 
 const int MPU = 0x68; // I2C address of the MPU6050 accelerometer
 
-int16_t Gyro::AcX, Gyro::AcY, Gyro::AcZ;
-int16_t Gyro::AcXOffset, Gyro::AcYOffset, Gyro::AcZOffset;
+bool Gyro::isPresent(false);
 
 void Gyro::startup()
 {
     // Initialize interface to the MPU6050
     LOGV1(DEBUG_INFO, F("GYRO:: Starting"));
     Wire.begin();
+
     Wire.beginTransmission(MPU);
-    Wire.write(0x6B);
-    Wire.write(0);
+    Wire.write(0x75);   // WHO_AM_I
+    Wire.endTransmission(true);
+    Wire.requestFrom(MPU, 1, 1);
+    byte id = (Wire.read() >> 1) & 0x3F;    
+    isPresent = (id == 0x34);
+    if (!isPresent) {
+        LOGV1(DEBUG_INFO, F("GYRO:: Not found!"));
+        return;
+    }
+
+    Wire.beginTransmission(MPU);
+    Wire.write(0x6B);   // PWR_MGMT_1
+    Wire.write(0);      // Disable sleep, 8 MHz clock
+    Wire.endTransmission(true);
+    Wire.beginTransmission(MPU);
+    Wire.write(0x1A);   // CONFIG
+    Wire.write(6);      // 5Hz bandwidth
     Wire.endTransmission(true);
 
-    // read the values 100 times for them to stabilize
-    for (int i = 0; i < 100; i++)
-    {
-        Wire.beginTransmission(MPU);
-        Wire.write(0x3B); // Start with register 0x3B (ACCEL_XOUT_H)
-        Wire.endTransmission(false);
-        Wire.requestFrom(MPU, 6, 1);       // Read 6 registers total, each axis value is stored in 2 registers
-        AcX = Wire.read() << 8 | Wire.read(); // X-axis value
-        AcY = Wire.read() << 8 | Wire.read(); // Y-axis value
-        AcZ = Wire.read() << 8 | Wire.read(); // Z-axis value
-    }
     LOGV1(DEBUG_INFO, F("GYRO:: Started"));
 }
 
@@ -48,20 +52,25 @@ angle_t Gyro::getCurrentAngles()
     struct angle_t result;
     result.pitchAngle = 0;
     result.rollAngle = 0;
+    if (!isPresent)
+        return result;     // Gyro is not available
+
     for (int i = 0; i < windowSize; i++)
     {
         Wire.beginTransmission(MPU);
         Wire.write(0x3B); // Start with register 0x3B (ACCEL_XOUT_H)
         Wire.endTransmission(false);
         Wire.requestFrom(MPU, 6, 1);       // Read 6 registers total, each axis value is stored in 2 registers
-        AcX = Wire.read() << 8 | Wire.read(); // X-axis value
-        AcY = Wire.read() << 8 | Wire.read(); // Y-axis value
-        AcZ = Wire.read() << 8 | Wire.read(); // Z-axis value
+        int16_t AcX = Wire.read() << 8 | Wire.read(); // X-axis value
+        int16_t AcY = Wire.read() << 8 | Wire.read(); // Y-axis value
+        int16_t AcZ = Wire.read() << 8 | Wire.read(); // Z-axis value
 
         // Calculating the Pitch angle (rotation around Y-axis)
         result.pitchAngle += ((atan(-1 * AcX / sqrt(pow(AcY, 2) + pow(AcZ, 2))) * 180.0 / PI) * 2.0) / 2.0;
         // Calculating the Roll angle (rotation around X-axis)
         result.rollAngle += ((atan(-1 * AcY / sqrt(pow(AcX, 2) + pow(AcZ, 2))) * 180.0 / PI) * 2.0) / 2.0;
+
+        delay(10);  // Decorrelate measurements
     }
 
     result.pitchAngle /= windowSize;
@@ -76,6 +85,9 @@ angle_t Gyro::getCurrentAngles()
 
 float Gyro::getCurrentTemperature()
 {
+    if (!isPresent)
+        return 99.9;     // Gyro is not available
+
     // Read the temperature data
     float result = 0.0;
     int16_t tempValue;
