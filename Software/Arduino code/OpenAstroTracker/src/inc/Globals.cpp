086d11ed017375e2b415a5edf1c82dc7fa4a8429
Andre Stefanov <mail@andrestefanov.de>
2020-12-06 21:52:09 +0100

    fixed build

--- Software/Arduino code/OpenAstroTracker/src/inc/Globals.cpp
+++ Software/Arduino code/OpenAstroTracker/src/inc/Globals.cpp
@@ -4,4 +4,4 @@
 // TODO (AS): This is just a tmp fix, we need to get rid of these globals
 
 float RAStepsPerDegree = RA_STEPS_PER_DEGREE;
-int DECStepsPerDegree = DEC_STEPS_PER_DEGREE;
+float DECStepsPerDegree = DEC_STEPS_PER_DEGREE;
